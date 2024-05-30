10a9a374f02e1b98de63fb49723c9116b1959b95
Gaurav-822 <gaurav.moocs@gmail.com>
2024-05-30 10:58:39 +0530

    UI improvement

--- lib/widgets/custom_bottom_navbar/custom_bottom_navbar.dart
+++ lib/widgets/custom_bottom_navbar/custom_bottom_navbar.dart
@@ -3,33 +3,47 @@ import 'package:flutter/material.dart';
 class CustomBottomNavbar extends StatelessWidget {
   final int selected;
   final Function onTap;
-  const CustomBottomNavbar(
-      {super.key, required this.selected, required this.onTap});
+  final bool isVisible;
+
+  const CustomBottomNavbar({
+    Key? key,
+    required this.selected,
+    required this.onTap,
+    required this.isVisible,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: const Color.fromARGB(255, 0, 45, 82),
-        selectedItemColor: Colors.purple,
-        // backgroundColor: Color.fromARGB(192, 1, 255, 56),
-        showSelectedLabels: true,
-        showUnselectedLabels: false,
-        currentIndex: selected,
-        onTap: (index) => onTap(index),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "Explore",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.my_location),
-            label: "Space",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit_location_alt),
-            label: "Create",
-          ),
-        ]);
+    return Visibility(
+      visible: isVisible,
+      child: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          height: 60, // Adjust the height as needed
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        ),
+        child: NavigationBar(
+          selectedIndex: selected,
+          onDestinationSelected: (index) => onTap(index),
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.search),
+              label: "Explore",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.my_location),
+              label: "Space",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.edit_location_alt),
+              label: "Create",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
