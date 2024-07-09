84f98240b10ad541e2d93a71fdc4fe8d92060fb9
Gaurav-822 <gaurav.moocs@gmail.com>
2024-07-09 18:48:48 +0530

    Receiving Data from Unity

--- lib/pages/create_page/create_page.dart
+++ lib/pages/create_page/create_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart'; // Import Fluttertoast package
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
 class CreatePage extends StatelessWidget {
-  CreatePage({super.key});
+  CreatePage({Key? key});
 
   static final GlobalKey<ScaffoldState> _scaffoldKey =
       GlobalKey<ScaffoldState>();
@@ -13,35 +14,49 @@ class CreatePage extends StatelessWidget {
     _unityWidgetController = controller;
   }
 
+  // Callback to handle messages from Unity
+  void onUnityMessageHandler(message) {
+    // Display received data in a toast
+    Fluttertoast.showToast(
+      msg: "Received data from Unity: $message",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Flexible(
-          child: Align(
-            alignment: AlignmentDirectional(0, -1),
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                child: Stack(children: [
-                  UnityWidget(
-                    onUnityCreated: onUnityCreated,
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            child: Align(
+              alignment: AlignmentDirectional(0, -1),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).secondaryHeaderColor,
+                    borderRadius: BorderRadius.circular(24),
                   ),
-                ]),
+                  child: UnityWidget(
+                    onUnityCreated: onUnityCreated,
+                    onUnityMessage:
+                        onUnityMessageHandler, // Pass the handler function here
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
